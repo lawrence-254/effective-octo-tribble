62eb52d2fc4e2a5ab37bd361a33b9bc31f4c4b1f
lawrence-254 <lodhiambo26@gmail.com>
2024-07-05 23:04:59 +0300

    setup

---
+++ mobile/app/(screens)/(auth)/profile.tsx
@@ -0,0 +1,38 @@
+import {Text} from 'react-native'
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+
+export default function Profile (){
+	return(
+		 <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+		<Text>hello prof</Text>
+		</ParallaxScrollView>
+	)
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
---
+++ mobile/app/index.jsx
@@ -0,0 +1,8 @@
+import {Text, View} from'react-native';
+export default function App (){
+	return (
+		<View>
+		<Text>Hello home</Text>
+		</View>
+		)
+}
