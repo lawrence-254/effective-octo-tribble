35f2f097bf82efbba0bb3c73a8bbc3395207bbf7
lawrence-254 <lodhiambo26@gmail.com>
2024-07-09 11:27:43 +0300

    resetformsandroutes

--- api/app/forms.py
+++ api/app/forms.py
@@ -2,6 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
+'''
+auth forms
+including:
+    registration
+    login
+    details reset
+'''
+
+'''
+registration class form
+'''
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired(), Length(min=6, max=12)])
@@ -9,7 +20,50 @@ class RegisterForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
 
+'''
+login class form
+'''
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=6, max=12)])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+'''
+resert uername class form
+'''
+class ResetUsernameForm(FlaskForm):
+    new_username = StringField('New Username', validators=[DataRequired(), Length(min=6, max=12)])
+    submit = SubmitField('Reset Username')
+
+'''
+reset email class form
+'''
+class ResetEmailForm(FlaskForm):
+    new_email = StringField('New Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Reset Email')
+
+'''
+reset password class form
+'''
+class ResetPasswordForm(FlaskForm):
+    new_password = PasswordField('New Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('new_password', message='Passwords must match')])
+    submit = SubmitField('Reset Password')
+
+'''end of auth forms'''
+
+'''
+Data entry forms
+includes:
+    catagory class form for inputin CategoryForm
+    journal class form for data entry
+'''
+
+'''category class form'''
+class CategoryForm(FlaskForm):
+    category = StringField('Category', validators=[DataRequired()])
+
+'''journal form class'''
+class JournalEntryForm(FlaskForm)
+
+
--- api/app/routes.py
+++ api/app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import app, db
-from app.forms import RegisterForm, LoginForm
+from app.forms import RegisterForm, LoginForm, ResetUsernameForm, ResetEmailForm, ResetPasswordForm
 from app.models import User
 
 @app.route('/')
@@ -66,4 +66,53 @@ def logout():
     session.pop('username', None)
     flash('You have been logged out.', 'success')
     return redirect(url_for('index'))
+
+
+@app.route('/reset_username', methods=['GET', 'POST'])
+def reset_username():
+    if 'user_id' not in session:
+        flash('You need to be logged in to reset your username.', 'danger')
+        return redirect(url_for('login'))
+
+    form = ResetUsernameForm()
+    if form.validate_on_submit():
+        user = User.query.get(session['user_id'])
+        user.username = form.new_username.data
+        db.session.commit()
+        flash('Your username has been updated!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('reset_username.html', title='Reset Username', form=form)
+
+@app.route('/reset_email', methods=['GET', 'POST'])
+def reset_email():
+    if 'user_id' not in session:
+        flash('You need to be logged in to reset your email.', 'danger')
+        return redirect(url_for('login'))
+
+    form = ResetEmailForm()
+    if form.validate_on_submit():
+        user = User.query.get(session['user_id'])
+        user.email = form.new_email.data
+        db.session.commit()
+        flash('Your email has been updated!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('reset_email.html', title='Reset Email', form=form)
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    if 'user_id' not in session:
+        flash('You need to be logged in to reset your password.', 'danger')
+        return redirect(url_for('login'))
+
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user = User.query.get(session['user_id'])
+        user.hashed_password = generate_password_hash(form.new_password.data)
+        db.session.commit()
+        flash('Your password has been updated!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('reset_password.html', title='Reset Password', form=form)
 
