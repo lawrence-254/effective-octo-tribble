49a8a881c233f1805460775f10a1e1dfe8997c7d
lawrence-254 <lodhiambo26@gmail.com>
2024-07-11 21:41:10 +0300

    foldesetup

---
+++ mobile/app/(auth)/reset_credentials.tsx
@@ -0,0 +1,44 @@
+import {Text, View} from 'react-native'
+
+const RestPassword = ()=>{
+    return(
+        <View>
+        <Text>
+        pass
+        </Text>
+        </View>
+    )
+}
+
+const ResetUsername = () =>{
+    return(
+        <View>
+        <Text>
+        user
+        </Text>
+        </View>
+    )
+}
+
+const ResetEmail = () => {
+    return(
+        <View>
+        <Text>
+        ResetEmail
+        </Text>
+        </View>
+    )
+}
+const ResetCredentials = ()=>{
+
+    return(
+         <View>
+        <Text>
+       reset
+        </Text>
+        </View>
+    )
+}
+
+
+export default ResetCredentials
